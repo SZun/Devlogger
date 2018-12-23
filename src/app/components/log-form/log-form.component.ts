2e76528b8714b956b9ee0162a0d40678b75fa0b3
Zun <zunsamuel97@gmail.com>
2018-12-23 13:44:11 -0500

    moving to persistence

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { LogService } from '../../services/log.service';
+import { LogService } from "../../services/log.service";
 
-import { Log } from '../../models/Log';
+import { Log } from "../../models/Log";
 
 @Component({
-  selector: 'app-log-form',
-  templateUrl: './log-form.component.html',
-  styleUrls: ['./log-form.component.css']
+  selector: "app-log-form",
+  templateUrl: "./log-form.component.html",
+  styleUrls: ["./log-form.component.css"]
 })
 export class LogFormComponent implements OnInit {
   id: string;
@@ -16,12 +16,12 @@ export class LogFormComponent implements OnInit {
 
   isNew: boolean = true;
 
-  constructor(private logService: LogService) { }
+  constructor(private logService: LogService) {}
 
   ngOnInit() {
     // Subscribe to the selectedLog observable
     this.logService.selectedLog.subscribe(log => {
-      if(log.id !== null) {
+      if (log.id !== null) {
         this.isNew = false;
         this.id = log.id;
         this.text = log.text;
@@ -32,13 +32,13 @@ export class LogFormComponent implements OnInit {
 
   onSubmit() {
     // Check if new log
-    if(this.isNew) {
+    if (this.isNew) {
       // Create a new log
       const newLog = {
         id: this.generateId(),
         text: this.text,
         date: new Date()
-      }
+      };
       // Add log
       this.logService.addLog(newLog);
     } else {
@@ -47,17 +47,27 @@ export class LogFormComponent implements OnInit {
         id: this.id,
         text: this.text,
         date: new Date()
-      }
+      };
       // Update log
       this.logService.updateLog(updLog);
     }
+    // Clear state
+    this.clearState();
   }
 
   generateId() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+      var r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
       return v.toString(16);
     });
   }
 
+  clearState() {
+    this.isNew = true;
+    this.id = "";
+    this.text = "";
+    this.date = "";
+    this.logService.clearState();
+  }
 }
